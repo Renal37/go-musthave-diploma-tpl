27b648525841313f244eba1aaf0907d56cf4ec75
Renal37 <renalplaycsgo@gmail.com>
2024-10-05 13:41:33 +0300

    На проверку

--- internal/database/migrations/2_add_orders.up.sql
+++ internal/database/migrations/2_add_orders.up.sql
@@ -1,14 +1,18 @@
 CREATE TYPE order_status AS ENUM ('NEW', 'PROCESSING', 'INVALID', 'PROCESSED');
 
 CREATE TABLE orders (
-    id          text PRIMARY KEY,
-    user_id     uuid REFERENCES users NOT NULL,
-    status      order_status NOT NULL DEFAULT 'NEW',
-    uploaded_at timestamp NOT NULL DEFAULT current_timestamp
+    id          text PRIMARY KEY,                             -- Уникальный идентификатор заказа
+    user_id     uuid REFERENCES users NOT NULL,              -- Идентификатор пользователя (внешний ключ на таблицу users)
+    status      order_status NOT NULL DEFAULT 'NEW',         -- Статус заказа
+    uploaded_at timestamp NOT NULL DEFAULT current_timestamp  -- Время загрузки заказа
 );
 
+CREATE INDEX idx_orders_user_id ON orders (user_id);
+
 CREATE TABLE accrual_flow (
-    id       uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    order_id text REFERENCES orders NOT NULL,
-    amount   numeric(15, 2) NOT NULL DEFAULT 0
+    id       uuid PRIMARY KEY DEFAULT uuid_generate_v4(),  -- Уникальный идентификатор потока начислений
+    order_id text REFERENCES orders NOT NULL,                -- Идентификатор заказа (внешний ключ на таблицу orders)
+    amount   numeric(15, 2) NOT NULL DEFAULT 0               -- Сумма начисления
 );
+
+CREATE INDEX idx_accrual_flow_order_id ON accrual_flow (order_id);
